a5ba2f62cf3c7a443413ea113aef6a9417b300d3
reeha <56428237+syedareehaquasar@users.noreply.github.com>
2021-03-14 12:02:46 +0530

    Create 1790. Check if One String Swap Can Make Strings Equal.py

---
+++ 1790. Check if One String Swap Can Make Strings Equal.py
@@ -0,0 +1,20 @@
+"""You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
+
+Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false."""
+
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        if s1 == s2:
+            return True
+        if len(s1) != len(s2):
+            return False
+        notMatch = 0
+        for n, letters in enumerate(s1):
+            if letters not in s2:
+                return False
+            if notMatch > 2:
+                return False
+            if letters != s2[n]:
+                notMatch += 1
+        return True
+            
