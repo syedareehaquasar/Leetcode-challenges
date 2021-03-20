a75fe3fd6a0113ba6e1c3d8009aabcdacbf01a6a
reeha <56428237+syedareehaquasar@users.noreply.github.com>
2021-03-20 10:23:09 +0530

    Update mountainArray.py

--- mountainArray.py
+++ mountainArray.py
@@ -3,7 +3,7 @@ class Solution:
         if len(arr) < 3:
             return False
         peak = 0
-        for i in range(1, len(arr) - 2):
+        for i in range(1, len(arr) - 1):
             if arr[i - 1] < arr[i] > arr[i + 1]:
                 peak += 1
             if arr[i - 1] >= arr[i] <= arr[i + 1]:
