302b6e0e4b1031a1501b7406f3b662b812b0b0ac
reeha <56428237+syedareehaquasar@users.noreply.github.com>
2021-04-04 21:00:36 +0530

    Create primeDiff.py

---
+++ primeDiff.py
@@ -0,0 +1,35 @@
+def firstFactor(n : int, k : int) -> int:
+    if n % k == 0:
+        return k
+    if (k * k) > n:
+        return n
+    return firstFactor(n, k+1)
+
+def isPrime(n : int) -> bool:
+    if n < 2:
+        return False
+    if n in [2, 3, 5, 7]:
+        return True
+    if n % 2 == 0:
+        return False
+    return firstFactor(n, 3) == n
+
+tc = int(input())
+for z in range(tc):
+    l, r = map (int, input().split())
+    if l > r:
+        l, r = r, l
+    prime1, prime2 = 0, 0
+    for x in range(l, r+1):
+        if isPrime(x):
+            prime1 = x
+            break
+    for x in reversed(range(l, r+1)):
+        if isPrime(x):
+            prime2 = x
+            break
+    if prime1 == 0 or prime2 == 0:
+        print(-1)
+    else:
+        print(prime2 - prime1)
+
